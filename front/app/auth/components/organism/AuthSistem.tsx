326d9f2dfec13c235142e0b79f23640bd7cc4bc9
Diego GP <diego.gp227@gmail.com>
2026-03-26 16:01:28 +0000

    [ style ] login form stiliced

--- front/app/auth/components/organism/AuthSistem.tsx
+++ front/app/auth/components/organism/AuthSistem.tsx
@@ -8,11 +8,24 @@ export default function AuthSistem() {
   const [isLogin, setIsLogin] = useState<boolean>(true);
 
   return (
-    <div className="bg-base w-full p-5">
+    <div className="bg-base w-full p-5 flex flex-col justify-evenly">
       <div className="gap-20 flex w-full justify-center">
-        <SelectAuthSistem isLogin={isLogin} setIsLogin={setIsLogin}/>
+        <SelectAuthSistem isLogin={isLogin} setIsLogin={setIsLogin} />
       </div>
-      <div className="text-shadow-priority-medium">
+      <div className="text-shadow-priority-medium p-5 flex flex-col gap-7">
+        <div className="w-full flex flex-col items-center">
+          {isLogin ? (
+            <>
+              <h2 className="font-bold text-3xl">Welcome back</h2>
+              <p className="text-fg-muted">Continue where you left off.</p>
+            </>
+          ) : (
+            <>
+              <h2 className="font-bold text-3xl">Register</h2>
+              <p>Continue where you left off.</p>
+            </>
+          )}
+        </div>
         {isLogin ? <LoginForm /> : <div>register</div>}
       </div>
     </div>
--- front/app/auth/components/organism/LoginForm.tsx
+++ front/app/auth/components/organism/LoginForm.tsx
@@ -1,29 +1,41 @@
 import { useForm } from "react-hook-form";
 
 export default function LoginForm() {
-  const { register, clearErrors, handleSubmit } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
   function test(data: any) {
     console.log(data);
   }
 
   return (
-    <form onSubmit={handleSubmit(test)}>
-      <input
-        type="text"
-        {...register("one")}
-        className="border-2 border-border"
-      />
-      <input
-        type="text"
-        {...register("two")}
-        className="border-2 border-border"
-      />
-      <input
-        type="text"
-        {...register("three")}
-        className="border-2 border-border"
-      />
-      <button type="submit" className="border-2 border-border w-5 h-5"></button>
+    <form
+      onSubmit={handleSubmit(test)}
+      className="flex justify-center flex-col gap-6"
+    >
+      <div className="flex flex-col">
+        <label htmlFor="email" className="text-fg-muted">Email</label>
+        <input
+          type="text"
+          id="email"
+          {...register("one")}
+          className="border-2 border-border px-1 py-2 rounded focus:outline-none focus:border-blue-600 transition-all duration-500"
+        />
+      </div>
+      <div className="flex flex-col">
+        <label htmlFor="password" className="text-fg-muted">Password</label>
+        <input
+          type="password"
+          id="password"
+          {...register("two")}
+          className="border-2 border-border px-1 py-2 rounded focus:outline-none focus:border-blue-600 transition-all duration-500"
+        />
+      </div>
+      <button type="submit" className="border-2 border-border px-1 py-2 bg-blue-600 hover:bg-blue-700 transition-all duration-500">
+        Send
+      </button>
     </form>
   );
 }
