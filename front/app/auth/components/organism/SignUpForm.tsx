b48511dc2e6eefe09769fc42168a29588a8e0248
Diego GP <diego.gp227@gmail.com>
2026-03-31 23:50:11 +0000

    [ feat ] signup integrated in form

--- front/app/auth/components/organism/SignUpForm.tsx
+++ front/app/auth/components/organism/SignUpForm.tsx
@@ -1,3 +1,6 @@
+import { useSignUp } from "@/src/auth/hooks/useSignUp";
+import { ICreateUserRequest } from "@/src/auth/types/auth.types";
+import { useRouter } from "next/navigation";
 import { useForm } from "react-hook-form";
 
 export default function SignUpForm() {
@@ -5,19 +8,41 @@ export default function SignUpForm() {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm();
+  } = useForm<ICreateUserRequest>();
+
+  const { error, loading, signup } = useSignUp();
+
+  const router = useRouter();
+
+  const onSubmit = async (data: ICreateUserRequest) => {
+    const success = await signup(data);
+
+    if (success) {
+      router.push("/");
+    }
+  };
 
   return (
-    <form action="" className="flex justify-center flex-col w-full gap-5">
+    <form
+      action=""
+      className="flex justify-center flex-col w-full gap-5"
+      onSubmit={handleSubmit(onSubmit)}
+    >
       <div className="flex w-full gap-5">
         <div className="flex flex-col flex-1 min-w-0">
-          <label htmlFor="name" className="text-fg-muted">Name</label>
+          <label htmlFor="name" className="text-fg-muted">
+            Name
+          </label>
           <input
             id="name"
             type="text"
             className="w-full border-2 border-border px-1 py-2 rounded focus:outline-none focus:border-blue-600 transition-all duration-500"
             {...register("name")}
+            disabled={loading}
           />
+          {errors.name && (
+            <p className="text-red-500 text-sm">{errors.name.message}</p>
+          )}
         </div>
 
         <div className="flex flex-col flex-1 min-w-0">
@@ -29,7 +54,11 @@ export default function SignUpForm() {
             type="text"
             className="w-full border-2 border-border px-1 py-2 rounded focus:outline-none focus:border-blue-600 transition-all duration-500"
             {...register("username")}
+            disabled={loading}
           />
+          {errors.username && (
+            <p className="text-red-500 text-sm">{errors.username.message}</p>
+          )}
         </div>
       </div>
       <div className="flex flex-col">
@@ -37,24 +66,34 @@ export default function SignUpForm() {
           Email
         </label>
         <input
+          id="email"
           type="email"
           className="border-2 border-border px-1 py-2 rounded focus:outline-none focus:border-blue-600 transition-all duration-500"
           {...register("email")}
+          disabled={loading}
         />
       </div>
       <div className="flex flex-col">
-        <label htmlFor="password" className="text-fg-muted">Password</label>
+        <label htmlFor="password" className="text-fg-muted">
+          Password
+        </label>
         <input
+          id="password"
           type="password"
           className="border-2 border-border px-1 py-2 rounded focus:outline-none focus:border-blue-600 transition-all duration-500"
           {...register("password")}
+          disabled={loading}
         />
       </div>
+
+      {error && <p className="text-red-500 text-sm">{error}</p>}
+
       <button
         type="submit"
         className="border-2 border-border px-1 py-2 bg-blue-600 hover:bg-blue-700 transition-all duration-500"
+        disabled={loading}
       >
-        Send
+        {loading ? "loading..." : "send"}
       </button>
     </form>
   );
