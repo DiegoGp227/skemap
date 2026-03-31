aed492a0f0de3d1fc8cc66593e2955b0feb47b86
Diego GP <diego.gp227@gmail.com>
2026-03-31 23:52:09 +0000

    [ feat ] url signup integrated

--- front/src/shared/constants/urls.ts
+++ front/src/shared/constants/urls.ts
@@ -1,4 +1,7 @@
 export const BaseURL =
   process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:8000/api/";
 
+//AUTH
 export const LoginURL = new URL("login", BaseURL);
+
+export const SignUp = new URL("signup", BaseURL);
