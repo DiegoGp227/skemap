be8e51892e969eb50bf1d3bad2bce2120a32b505
Diego GP <diego.gp227@gmail.com>
2026-03-31 22:35:04 +0000

    [ feat ] login service integrated

--- back/src/modules/auth/auth.services.ts
+++ back/src/modules/auth/auth.services.ts
@@ -1,8 +1,11 @@
 import bcrypt from "bcryptjs";
 import jwt, { SignOptions } from "jsonwebtoken";
-import { EmailAlreadyInUseError } from "../../errors/businessErrors";
+import {
+  EmailAlreadyInUseError,
+  InvalidCredentialsError,
+} from "../../errors/businessErrors";
 import prisma from "../../db/prisma.js";
-import { ICreateUser, IUserResponse } from "./auth.types";
+import { IAuthUser, ICreateUser, IUserResponse } from "./auth.types";
 
 export const createUser = async (
   userData: ICreateUser,
@@ -33,6 +36,46 @@ export const createUser = async (
       updatedAt: true,
     },
   });
+
+  const token = jwt.sign(
+    { id: user.id, email: user.email },
+    process.env.JWT_SECRET || "default_secret",
+    {
+      expiresIn: (process.env.TOKEN_EXPIRATION || "1h") as string,
+    } as SignOptions,
+  );
+
+  return { user, token };
+};
+
+export const validateUser = async (
+  userData: IAuthUser,
+): Promise<{ user: IUserResponse; token: string }> => {
+  const existingUser = await prisma.user.findUnique({
+    where: { email: userData.email },
+  });
+
+  if (!existingUser) {
+    throw new InvalidCredentialsError(userData.email);
+  }
+
+  const isPasswordValid = await bcrypt.compare(
+    userData.password,
+    existingUser.password,
+  );
+
+  if (!isPasswordValid) {
+    throw new InvalidCredentialsError(userData.email);
+  }
+
+  const user = {
+    id: existingUser.id,
+    name: existingUser.name,
+    username: existingUser.username,
+    email: existingUser.email,
+    createdAt: existingUser.createdAt,
+    updatedAt: existingUser.updatedAt,
+  };
 
   const token = jwt.sign(
     { id: user.id, email: user.email },
