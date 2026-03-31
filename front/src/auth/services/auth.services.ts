2cc9182a46f13e3e9b8a16e8b4ea9df1a96d6619
Diego GP <diego.gp227@gmail.com>
2026-03-31 23:51:15 +0000

    [ feat ] signup service integrated

--- front/src/auth/services/auth.services.ts
+++ front/src/auth/services/auth.services.ts
@@ -1,12 +1,31 @@
 import { postFetcher } from "@/utils/utils";
-import { IAuthResponse, ICredentials } from "../types/auth.types";
-import { LoginURL } from "@/src/shared/constants/urls";
+import {
+  IAuthResponse,
+  ICreateUserRequest,
+  ICredentials,
+} from "../types/auth.types";
+import { LoginURL, SignUp } from "@/src/shared/constants/urls";
 
-export function loginService(credentials: ICredentials): Promise<IAuthResponse> {
+export function loginService(
+  credentials: ICredentials,
+): Promise<IAuthResponse> {
   const url = LoginURL.toString();
-  const response = postFetcher<IAuthResponse>(
-    url,
-    { email: credentials.email, password: credentials.password },
-  );
+  const response = postFetcher<IAuthResponse>(url, {
+    email: credentials.email,
+    password: credentials.password,
+  });
+  return response;
+}
+
+export function SignUpService(
+  credentials: ICreateUserRequest,
+): Promise<IAuthResponse> {
+  const url = SignUp.toString();
+  const response = postFetcher<IAuthResponse>(url, {
+    email: credentials.email,
+    password: credentials.password,
+    username: credentials.username,
+    name: credentials.name,
+  });
   return response;
 }
