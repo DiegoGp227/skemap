cb44ebac32f481a0fa17211a23ea1b8581e2cf1f
Diego GP <diego.gp227@gmail.com>
2026-03-31 22:40:35 +0000

    [ feat ] login integrated in routes

--- back/src/routes/index.routes.ts
+++ back/src/routes/index.routes.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import dbCheck from "../modules/test/test.js";
-import { signup } from "../modules/auth/auth.controllers.js";
+import { login, signup } from "../modules/auth/auth.controllers.js";
 
 export const router: Router = Router();
 
@@ -9,3 +9,4 @@ router.get("/db", dbCheck);
 
 // auth routes
 router.post("/signup", signup);
+router.post("/login", login);
