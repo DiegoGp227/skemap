43dd28e6ac819014d39f51bc099c465c54a1167d
Diego GP <diego.gp227@gmail.com>
2026-04-01 15:09:32 +0000

    [ feat ] home sistem change to filter form

--- front/app/components/organism/HomeSistem.tsx
+++ front/app/components/organism/HomeSistem.tsx
@@ -1,10 +1,28 @@
+"use client";
 import FilterForm from "@/src/home/forms/FilterForm";
 import ProjectsSection from "./ProjectsSection";
+import { useState } from "react";
 
 export default function HomeSistem() {
+  const [status, setStatus] = useState<string>("all");
+  const [search, setSearch] = useState<string>("");
+
+  function ChangeFilterStatus(status: string) {
+    setStatus(status);
+    console.log(status);
+  }
+
+  function ChangeFilterSearch(search: string) {
+    setSearch(search);
+    console.log(search);
+  }
+
   return (
     <>
-      <FilterForm />
+      <FilterForm
+        onFilterStatus={ChangeFilterStatus}
+        onFilterSearch={ChangeFilterSearch}
+      />
       <ProjectsSection />
     </>
   );
