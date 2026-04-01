da57e2867867c6b910995635bb78611ae8bbbb0b
Diego GP <diego.gp227@gmail.com>
2026-04-01 10:08:57 +0000

    [ feat ] get proyects integrated in routes

--- back/src/routes/index.routes.ts
+++ back/src/routes/index.routes.ts
@@ -1,12 +1,16 @@
 import { Router } from "express";
 import dbCheck from "../modules/test/test.js";
 import { login, signup } from "../modules/auth/auth.controllers.js";
+import { getProjects } from "../modules/projects/projects.controllers.js";
 
 export const router: Router = Router();
 
-// Test routes
+// Test Routes
 router.get("/db", dbCheck);
 
-// auth routes
+// Auth Routes
 router.post("/signup", signup);
 router.post("/login", login);
+
+// Projects Routes
+router.get("/projects", getProjects);
