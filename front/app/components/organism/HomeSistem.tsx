7e29a07259333499fe1a6e5025b8c252b3934ab3
Diego GP <diego.gp227@gmail.com>
2026-04-01 15:51:51 +0000

    [ feat ] home sistem adaptated for data

--- front/app/components/organism/HomeSistem.tsx
+++ front/app/components/organism/HomeSistem.tsx
@@ -1,29 +1,23 @@
 "use client";
 import FilterForm from "@/src/home/forms/FilterForm";
 import ProjectsSection from "./ProjectsSection";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import useProjects from "@/src/home/hooks/useProjects";
 
 export default function HomeSistem() {
   const [status, setStatus] = useState<string>("all");
   const [search, setSearch] = useState<string>("");
 
-  function ChangeFilterStatus(status: string) {
-    setStatus(status);
-    console.log(status);
-  }
+  const { error, loading, projects, fetchProjects } = useProjects();
 
-  function ChangeFilterSearch(search: string) {
-    setSearch(search);
-    console.log(search);
-  }
+  useEffect(() => {
+    fetchProjects(status, search);
+  }, [search, status, fetchProjects]);
 
   return (
     <>
-      <FilterForm
-        onFilterStatus={ChangeFilterStatus}
-        onFilterSearch={ChangeFilterSearch}
-      />
-      <ProjectsSection />
+      <FilterForm onFilterStatus={setStatus} onFilterSearch={setSearch} />
+      <ProjectsSection projects={projects} loading={loading} error={error} />
     </>
   );
 }
