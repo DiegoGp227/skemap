6cc110c0ec0ddfae43eebfde8ff8c3f864190fe2
Diego GP <diego.gp227@gmail.com>
2026-04-01 15:52:30 +0000

    [ wix ] home sistem in work for data

--- front/app/components/organism/ProjectsSection.tsx
+++ front/app/components/organism/ProjectsSection.tsx
@@ -1,15 +1,29 @@
+import { Project } from "@/src/home/types/home.types";
 import ProjectDiv from "../molecules/ProjectDiv";
 
-export default function ProjectsSection() {
+interface IProjectsSectionsProps {
+  projects: Project[];
+  loading: boolean;
+  error: string | null;
+}
+
+export default function ProjectsSection({
+  loading,
+  projects,
+  error,
+}: IProjectsSectionsProps) {
   return (
     <div className="flex gap-15 flex-wrap justify-center">
-      <ProjectDiv />
-      <ProjectDiv />
-      <ProjectDiv />
-      <ProjectDiv />
-      <ProjectDiv />
-      <ProjectDiv />
-      <ProjectDiv />
+      {loading ?? <p>"loading..."</p>}
+
+      {error ?? <p>{error}</p>}
+      {projects && projects.length > 0 ? (
+        projects.map((project) => <ProjectDiv key={project.id} />)
+      ) : (
+        <div>
+          <p>No hay proyectos</p>
+        </div>
+      )}
     </div>
   );
 }
