122947776ba1a187756fefa199f7b684dcb12379
Diego GP <diego.gp227@gmail.com>
2026-04-02 21:25:25 +0000

    [ feat ] projects secition update

--- front/app/components/organism/ProjectsSection.tsx
+++ front/app/components/organism/ProjectsSection.tsx
@@ -14,16 +14,44 @@ export default function ProjectsSection({
 }: IProjectsSectionsProps) {
   return (
     <div className="flex gap-15 flex-wrap justify-center">
-      {loading ?? <p>"loading..."</p>}
-
-      {error ?? <p>{error}</p>}
+      {loading && <p>"loading..."</p>}
+      {error && <p>{error}</p>}
       {projects && projects.length > 0 ? (
-        projects.map((project) => <ProjectDiv key={project.id} />)
+        projects.map((project) => (
+          <ProjectDiv
+            key={project.id}
+            status={project.status}
+            description={project.description}
+            title={project.name}
+          />
+        ))
       ) : (
-        <div>
-          <p>No hay proyectos</p>
+        <div className="w-100 h-35 p-5 border-2 bg-surface border-border rounded transition duration-500 ease-in-out hover:border-ring flex flex-col gap-5 justify-center items-center">
+          <p>Projects Not Found</p>
+          <button
+            className="bg-blue-600 py-2 px-10 cursor-pointer rounded"
+            onClick={() => {
+              alert("hello");
+            }}
+          >
+            New Project
+          </button>
         </div>
       )}
+      {/* <ProjectDiv></ProjectDiv>
+      <ProjectDiv></ProjectDiv>
+      <ProjectDiv></ProjectDiv>
+      <ProjectDiv></ProjectDiv>
+      <ProjectDiv></ProjectDiv>
+      <ProjectDiv></ProjectDiv>
+      <ProjectDiv></ProjectDiv>
+      <ProjectDiv></ProjectDiv> 
+      <ProjectDiv></ProjectDiv>
+      <ProjectDiv></ProjectDiv>
+      <ProjectDiv></ProjectDiv> 
+      <ProjectDiv></ProjectDiv>
+      <ProjectDiv></ProjectDiv>
+      <ProjectDiv></ProjectDiv> */}
     </div>
   );
 }
