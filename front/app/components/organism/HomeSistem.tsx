ac507ee41b50fb01bc3a4d6bea63ab5db18aca2c
Diego GP <diego.gp227@gmail.com>
2026-04-02 23:18:58 +0000

    [ feat ] home sistem state form integrated

--- front/app/components/organism/HomeSistem.tsx
+++ front/app/components/organism/HomeSistem.tsx
@@ -1,12 +1,14 @@
 "use client";
-import FilterForm from "@/src/home/forms/FilterForm";
+import FilterForm from "@/app/components/organism/FilterForm";
 import ProjectsSection from "./ProjectsSection";
 import { useEffect, useState } from "react";
 import useProjects from "@/src/home/hooks/useProjects";
+import NewProjectForm from "./NewProjectForm";
 
 export default function HomeSistem() {
   const [status, setStatus] = useState<string>("all");
   const [search, setSearch] = useState<string>("");
+  const [newProjectForm, setNewProjectForm] = useState<boolean>(false);
 
   const { error, loading, projects, fetchProjects } = useProjects();
 
@@ -17,7 +19,15 @@ export default function HomeSistem() {
   return (
     <>
       <FilterForm onFilterStatus={setStatus} onFilterSearch={setSearch} />
-      <ProjectsSection projects={projects} loading={loading} error={error} />
+      <ProjectsSection
+        projects={projects}
+        loading={loading}
+        error={error}
+        setNewProjectForm={setNewProjectForm}
+      />
+      {newProjectForm && (
+        <NewProjectForm onClose={() => setNewProjectForm(false)} />
+      )}
     </>
   );
 }
