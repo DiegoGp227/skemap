45c6e2ae456b3a450a720ba1da1465e809e4aeff
Diego GP <diego.gp227@gmail.com>
2026-04-02 21:23:00 +0000

    [ feat ] shema update

--- back/src/modules/projects/projects.shema.ts
+++ back/src/modules/projects/projects.shema.ts
@@ -10,5 +10,13 @@ export const updateProjectSchema = createProjectSchema.partial().extend({
   status: z.enum(["ACTIVE", "COMPLETED", "ARCHIVED"]).optional(),
 });
 
+// Query params del GET /projects
+export const getProjectsSchema = z.object({
+  // Los valores que el frontend puede enviar como status
+  status: z.enum(["all", "active", "complete", "filed"]).optional(),
+  search: z.string().optional(),
+});
+
 export type CreateProjectDTO = z.infer<typeof createProjectSchema>;
 export type UpdateProjectDTO = z.infer<typeof updateProjectSchema>;
+export type GetProjectsDTO = z.infer<typeof getProjectsSchema>;
