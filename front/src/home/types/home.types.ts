80c2a4df50ae813b2e0395df427fdbcdb8c82937
Diego GP <diego.gp227@gmail.com>
2026-04-03 20:56:36 +0000

    [ feat ] new types integrated in home

--- front/src/home/types/home.types.ts
+++ front/src/home/types/home.types.ts
@@ -1,6 +1,6 @@
 export type ProjectStatus = "ACTIVE" | "COMPLETED" | "ARCHIVED";
 
-export type ProjectColor = string; 
+export type ProjectColor = string;
 
 export interface Project {
   id: number;
@@ -14,4 +14,10 @@ export interface Project {
 
 export interface ProjectsResponse {
   projects: Project[];
+}
+
+export interface CreateProjectDto {
+  name: string;
+  color: string;
+  description: string;
 }
