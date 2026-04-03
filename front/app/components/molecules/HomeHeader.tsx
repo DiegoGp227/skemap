8a9afc9913d5c52e2ef46a66e41f51414bbb2572
Diego GP <diego.gp227@gmail.com>
2026-04-03 20:03:15 +0000

    [ refactor ] home header refactor

--- front/app/components/molecules/HomeHeader.tsx
+++ front/app/components/molecules/HomeHeader.tsx
@@ -1,11 +1,15 @@
-import HeaderProjectsInfo from "./HeaderProjectsInfo";
-
 export default function HomeHeader() {
   return (
-    <div className="flex justify-between h-full">
-      <HeaderProjectsInfo />
-      <div className="flex h-full items-center justify-center">
-        <button className="bg-blue-600 py-2 px-5 cursor-pointer rounded">New Project</button>
+    <div>
+      <div>
+        <h1 className="text-3xl text-fg">Projects</h1>
+      </div>
+      <div className="flex gap-2">
+        <p className="text-fg-muted">1 Actives</p>
+        <span className="text-fg-muted">•</span>
+        <p className="text-fg-muted">2 Complete</p>
+        <span className="text-fg-muted">•</span>
+        <p className="text-fg-muted">3 Archivate</p>
       </div>
     </div>
   );
