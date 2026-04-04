830704cc91a1ab216e088b1d94854140085cb740
Diego GP <diego.gp227@gmail.com>
2026-04-04 23:02:55 +0000

    [ feat ] home header update for data

--- front/app/components/molecules/HomeHeader.tsx
+++ front/app/components/molecules/HomeHeader.tsx
@@ -1,15 +1,26 @@
+"use client";
+
+import useProjectsStats from "@/src/home/hooks/useProjectsStats";
+
 export default function HomeHeader() {
+  const { error, loading, stats } = useProjectsStats();
   return (
     <div>
       <div>
         <h1 className="text-3xl text-fg">Projects</h1>
       </div>
       <div className="flex gap-2">
-        <p className="text-fg-muted">1 Actives</p>
-        <span className="text-fg-muted">•</span>
-        <p className="text-fg-muted">2 Complete</p>
-        <span className="text-fg-muted">•</span>
-        <p className="text-fg-muted">3 Archivate</p>
+        {loading ? (
+          "loading..."
+        ) : (
+          <>
+            <p className="text-fg-muted">{stats.active} Actives</p>
+            <span className="text-fg-muted">•</span>
+            <p className="text-fg-muted">{stats.completed} Complete</p>
+            <span className="text-fg-muted">•</span>
+            <p className="text-fg-muted">{stats.archived} Archivate</p>
+          </>
+        )}
       </div>
     </div>
   );
