bbebd48dbb851a3fe4bb60d03e52275c9001ff02
Diego GP <diego.gp227@gmail.com>
2026-04-04 13:33:00 +0000

    [ style ] minior changes

--- front/app/projects/components/organism/LateralBar.tsx
+++ front/app/projects/components/organism/LateralBar.tsx
@@ -5,7 +5,13 @@ import EpicSecction from "./EpicSecction";
 export default function LateralBar() {
   return (
     <section className="h-full max-w-72">
-      <InfoProject tecnologies="" title="Discipline App Discipline App Discipline AppDiscipline AppDiscipline App" color="#00FF00"  current={23} total={25}/>
+      <InfoProject
+        tecnologies=""
+        title="Discipline App Discipline App Discipline AppDiscipline AppDiscipline App"
+        color="#00FF00"
+        current={23}
+        total={25}
+      />
       <FilterProject />
       <EpicSecction />
     </section>
