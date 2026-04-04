bbcd97b45edb80f117b34db3f1c517d7c548631a
Diego GP <diego.gp227@gmail.com>
2026-04-04 13:21:13 +0000

    [ style ] style change

--- front/app/components/molecules/ProgressBar.tsx
+++ front/app/components/molecules/ProgressBar.tsx
@@ -2,21 +2,31 @@ interface IProgressBarProps {
   current: number;
   total: number;
   label: string;
+  color: string;
 }
 
-export default function ProgressBar({ current, label, total }: IProgressBarProps) {
+export default function ProgressBar({
+  current,
+  label,
+  total,
+  color,
+}: IProgressBarProps) {
   const percentage = total > 0 ? Math.min((current / total) * 100, 100) : 0;
 
   return (
-    <div className=" px-4 py-2.5 rounded-md">
+    <div className="py-2.5 rounded-md">
       <div className="flex justify-between mb-2">
-        <span className="text-[#9a9a9e] text-sm">{current}/{total} {label}</span>
-        <span className="text-amber-400 text-sm font-semibold">{Math.round(percentage)}%</span>
+        <span className="text-[#9a9a9e] text-sm">
+          {current}/{total} {label}
+        </span>
+        <span className="text-sm font-semibold" style={{ color: color }}>
+          {Math.round(percentage)}%
+        </span>
       </div>
       <div className="w-full h-1.5 bg-[#2a2a2e] rounded-full overflow-hidden">
         <div
-          className="h-full bg-amber-400 rounded-full"
-          style={{ width: `${percentage}%` }}
+          className="h-full rounded-full"
+          style={{ width: `${percentage}%`, backgroundColor: color }}
         />
       </div>
     </div>
