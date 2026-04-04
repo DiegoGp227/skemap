8cbd89f724563c63dca1f810f5ee85f41366a22f
Diego GP <diego.gp227@gmail.com>
2026-04-04 13:32:23 +0000

    [ style ] minior changes

--- front/app/projects/components/molecules/EpicDiv.tsx
+++ front/app/projects/components/molecules/EpicDiv.tsx
@@ -6,7 +6,7 @@ interface EpicDivProps {
 
 export default function EpicDiv({ color, title, progress }: EpicDivProps) {
   return (
-    <button className="w-full flex items-center justify-between cursor-pointer hover:bg-overlay hover:text-fg transition duration-500 rounded">
+    <button className="w-full flex items-center justify-between cursor-pointer hover:bg-overlay hover:text-fg transition duration-500 rounded py-1 px-3">
       <div
         className="w-3 h-3 rounded-full shrink-0"
         style={{ backgroundColor: color }}
