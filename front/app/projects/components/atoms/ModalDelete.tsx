1613f7ad1810228529426978e896ef714ddff39b
Diego GP <diego.gp227@gmail.com>
2026-04-12 16:56:01 +0000

    [ feat ] minior changes

---
+++ front/app/projects/components/atoms/ModalDelete.tsx
@@ -0,0 +1,15 @@
+export default function ModalDelete() {
+  return (
+    <div>
+      <div>
+        <p>
+          Are you sure you want to delete the project? The changes cannot be
+          undone.
+        </p>
+      </div>
+      <div>
+        <button>DELETE</button>
+      </div>
+    </div>
+  );
+}
